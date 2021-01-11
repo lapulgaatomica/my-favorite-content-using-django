51d5ed6eb12cd5523c60a8e8353fd13ac363f40b
pulga <akindeleodedoyin@gmail.com>
2021-01-11 21:07:52 +0100

    minor edit

--- favoritecontent_project/settings.py
+++ favoritecontent_project/settings.py
@@ -32,14 +32,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'content.apps.ContentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'content.apps.ContentConfig',
-    'whitenoise.runserver_nostatic', # new!
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 ]
 
