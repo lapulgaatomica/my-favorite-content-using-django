9033cbd8eeac18fd2c961d170a90762c2fff55ec
pulga <akindeleodedoyin@gmail.com>
2021-01-30 11:04:00 +0100

    added tests

--- content/tests.py
+++ content/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import DailyMailColumn
+import datetime
 
-# Create your tests here.
+class DailyMailColumnModelTest(TestCase):
+
+    def setUp(self):
+        self.column = DailyMailColumn.objects.create(
+            link='https://www.test.com',
+            title='test title',
+            columnist='test columnist'
+        )
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.column), self.column.title)
+
+    def test_column_content(self):
+        self.assertEqual(f'{self.column.link}', 'https://www.test.com')
+        self.assertEqual(f'{self.column.title}', 'test title')
+        self.assertEqual(f'{self.column.columnist}', 'test columnist')
+        self.assertTrue(isinstance(self.column.date_added, datetime.datetime))
+
+    def test_columns_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'test title')
+
+class HomePageViewTest(TestCase):
+
+    def test_view_url_exists_at_proper_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
